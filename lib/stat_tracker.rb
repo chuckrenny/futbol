69a062d2f3ea72cef9b617f21715c57590f9a1fa
Antoine Aube <aaube3@gmail.com>
2023-08-06 18:17:40 -0600

    feat: #average_win_pecentage, #percentage_games_won_by_team_id, #games_won_per_team

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -31,6 +31,22 @@ include SeasonStatable
     # 1. Iterate
   end
 
+  def games_won_per_team
+    @game_teams.each_with_object(Hash.new(0.0)) do |game, games_won|
+      games_won[game.team_id] += 1 if game. result == "WIN"
+    end
+  end
+
+  def percentage_games_won_by_team_id
+    games_won_per_team.each_with_object(Hash.new(0.0)) do |(key, value), hash|
+      hash[key] = (value / games_played_per_team[key]).round(2)
+    end
+  end
+
+  def average_win_percentage(team_id)
+    percentage_games_won_by_team_id[team_id]
+  end
+
   def self.from_csv(files)
     StatTracker.new(files)
   end
