2374550de7662883e7a3f7cd76ebf10ad320f3c1
Antoine Aube <aaube3@gmail.com>
2023-08-06 18:45:42 -0600

    refactor: add method #calculate_averageby_total_games

--- lib/game_statable.rb
+++ lib/game_statable.rb
@@ -12,30 +12,27 @@ module GameStatable
     @games.count { |game| game }.to_f
   end
 
+  def calculate_average_by_total_games(total)
+    (total / self.total_games).round(2).to_f
+  end
   def percentage_home_wins
-    home_wins = @game_teams.count do |game|
-      game.result == "WIN" && game.hoa == "home"
-    end
-    (home_wins / self.total_games).round(2).to_f
+    home_wins = @game_teams.count { |game| game.result == "WIN" && game.hoa == "home" }
+    calculate_average_by_total_games(home_wins)
   end
   
   def percentage_visitor_wins
-    visitor_wins = @game_teams.count do |game|
-      game.result == "WIN" && game.hoa == "away"
-    end
-    (visitor_wins / self.total_games).round(2).to_f
+    visitor_wins = @game_teams.count { |game| game.result == "WIN" && game.hoa == "away" }
+    calculate_average_by_total_games(visitor_wins)
   end
 
   def percentage_ties
-    ties = @game_teams.count do |game|
-      game.result == "TIE" && game.hoa == "away"
-    end
-    (ties / self.total_games).round(2).to_f
+    ties = @game_teams.count { |game| game.result == "TIE" && game.hoa == "away" }
+    calculate_average_by_total_games(ties)
   end
 
   def average_goals_per_game
     total_goals = @games.map { |game| game.away_goals + game.home_goals }.sum.to_f
-    (total_goals / @games.count).to_f.round(2)
+    calculate_average_by_total_games(total_goals)
   end
 
   def count_of_games_by_season
@@ -46,11 +43,9 @@ module GameStatable
     total_goals_by_season = @games.each_with_object(Hash.new(0.0)) { |game, hash| hash[game.season] += game.away_goals + game.home_goals}
   end 
 
-  def average_goals_by_season
-    average_goals_by_season = Hash.new(0)
-    total_goals_by_season.each do |key, value| 
-      average_goals_by_season[key] = (value / count_of_games_by_season[key]).round(2)
-    end
-    average_goals_by_season
-  end 
+  def average_goals_by_season 
+    total_goals_by_season.each_with_object(Hash.new(0.0)) do |(key, value), hash| 
+      hash[key] = (value / count_of_games_by_season[key]).round(2)
+    end 
+  end
 end
